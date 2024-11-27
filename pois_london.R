f2c22907d7e3c7be0a574adcf2e3511b46333ffc
tkrumrei <102969963+tkrumrei@users.noreply.github.com>
2024-11-27 14:02:55 +0100

    thames to pois map

--- pois_london.R
+++ pois_london.R
@@ -3,20 +3,22 @@ if (!require("sf")) install.packages("sf")
 if (!require("leaflet")) install.packages("leaflet")
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("htmlwidgets")) install.packages("htmlwidgets")
+if (!require("httr")) install.packages("httr")
 
 library(sf)
 library(leaflet)
 library(dplyr)
 library(htmlwidgets)
+library(httr)
 
-daten <- st_read("data/POIs London/pois_london.shp")
-print(daten)
-anzahl_types <- daten %>% 
+
+pois_london <- st_read("data/POIs London/pois_london.shp")
+anzahl_types <- pois_london %>% 
   group_by(type) %>% 
   summarise(anzahl = n())
 print(anzahl_types)
 
-bbox <- st_bbox(daten)
+bbox <- st_bbox(pois_london)
 buffer_factor <- 0.3
 bbox_small <- list(
   xmin = bbox[[1]] + (bbox[[3]] - bbox[[1]]) * buffer_factor,
@@ -25,14 +27,25 @@ bbox_small <- list(
   ymax = bbox[[4]] - (bbox[[4]] - bbox[[2]]) * buffer_factor
 )
 
+##### London outline #####
 greater_London_area <- st_read("data/London Boundaries/London_Ward.shp")
 shape_transformed <- st_transform(greater_London_area, crs = 4326)
 london_outline <- st_union(shape_transformed)
 london_outline_sf <- st_sf(geometry = london_outline)
 
+##### Thames Line #####
+url <- "https://gis2.london.gov.uk/server/rest/services/apps/webmap_context_layer/MapServer/1/query"
+params <- list(
+  where = "1=1",
+  outFields = "*",
+  f = "geojson"
+)
+response <- GET(url, query = params)
+linestring_data <- st_read(content(response, as = "text"), quiet = TRUE)
+
 '
 for (i in 1:4) {
-  typ_daten <- subset(daten, type == i)
+  typ_daten <- subset(pois_london, type == i)
   if (nrow(typ_daten) > 0) {
     karte <- leaflet(data = typ_daten) %>%
       addProviderTiles(providers$CartoDB.Positron) %>% 
@@ -51,9 +64,10 @@ for (i in 1:4) {
 }
 '
 
-karte_alle_types <- leaflet(data = daten) %>%
+karte_alle_types <- leaflet(data = pois_london) %>%
   addProviderTiles(providers$CartoDB.Positron) %>% 
   addPolygons(data = london_outline_sf, color = "black", weight = 1, opacity = 0.8, fill = FALSE, group = "London Outline") %>%
+  addPolylines(data = linestring_data, color = "blue", weight = 1, opacity = 0.5, group = "Thames Lines") %>%
   fitBounds(bbox_small$xmin, bbox_small$ymin, bbox_small$xmax, bbox_small$ymax) %>% 
   addPolygons(color = ~case_when(
     type == 1 ~ "#333333",
